71b55ac5ce04792a6e80da4bf812290c3369a0ea
William Valadez <billy.valadez@icloud.com>
2023-04-27 18:35:53 -0400

    made new function routeFunctions

--- step3.js
+++ step3.js
@@ -1,19 +1,25 @@
 const fs = require("fs");
 const axios = require("axios");
 
-const url = "https://" || "http://";
-const outFlagIndex = process.argv.indexOf("--out");
-const file =
-  outFlagIndex !== -1 && outFlagIndex + 1 < process.argv.length
-    ? process.argv[outFlagIndex + 1]
-    : null;
-const path = outFlagIndex === -1 ? process.argv[2] : process.argv[4];
+function routeFunctions() {
+  const url = "https://" || "http://";
+  const outIndex = process.argv.indexOf("--out");
 
-if (file !== null) {
-  path.includes(url) ? webCatWrite(path, file) : catWrite(path, file);
-} else {
-  path.includes(url) ? webCat(path) : cat(path);
+  const file =
+    outIndex !== -1 && outIndex + 1 < process.argv.length
+      ? process.argv[outIndex + 1]
+      : null;
+
+  const path = outIndex === -1 ? process.argv[2] : process.argv[4];
+
+  if (file !== null) {
+    path.includes(url) ? webCatWrite(path, file) : catWrite(path, file);
+  } else {
+    path.includes(url) ? webCat(path) : cat(path);
+  }
 }
+
+routeFunctions();
 
 function cat(path) {
   fs.readFile(`${path}`, "utf8", (error, data) => {
